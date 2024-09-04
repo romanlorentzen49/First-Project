aecf51641e8380893a80a762ad12861606eaadaa
Roman Lorentzen <romanlorentzen49@gmail.com>
2024-09-04 17:00:17 +0000

    continued

--- character.py
+++ character.py
@@ -1,17 +1,37 @@
+
+from wordle import makeAGuess
 class Character:
     def __init__(self, name, health):
         self.name = name
         self.health = health
         self.received = False
 
-    def talk_to_player(self,player_name, player_msg):
-        print("{self.name} says: {player_name}, welcome to the Celestial Jungle! Here is a cold drink for your travels.")
-        print("{player_name} drinks the cold drink and feels refreshed. Until {player_name} passes out. {self.name} tampered with the drink.")
 
-    def kidnap(player_name)
-        print("A few hours later, {player_name} wakes up in some bedroom. The room is dark and there is a door to the north. The door is locked. But you feel something dangling from the ceiling. You make out that it is a string.")
-        print("What do you do? (type 'pull' or 'leave')")
-        if input() == "pull":
-            print("You pull the string and a lightbulb turns on. You see a key on the ground. You pick it up and unlock the door. You leave the room.")
+
+def talk_to_player(self,player_name):
+    print("{self.name} says: {player_name}, welcome to the Celestial Jungle! Here is a cold drink for your travels.")
+    print("{player_name} drinks the cold drink and feels refreshed. Until {player_name} passes out. {self.name} tampered with the drink.")
+
+def kidnap(player_name):
+    print("A few hours later, {player_name} wakes up in some bedroom. The room is dark and there is a door to the north. The door is locked. But you feel something dangling from the ceiling. You make out that it is a string.")
+    print("What do you do? (type 'pull' or 'leave')")
+    if input() == "pull":
+        print("You pull the string and a lightbulb turns on. You find a compass, lockpick and you see that the lock isn't a traditional lock. You figure out that it is some sort of wordle game.")
+        makeAGuess(users_guess)
+    else:
+        print("You start feel around for any objects you can find. You find an oddly shaped lock on the door. You feel around and find a lock pick. You successfully pick the lock and escape the room.")
+    print("You feel a lock on the door and find a compass and a lock pick on the floor. Do you want to try and break the lock with the rock or try and pick the lock.(type 'break' or 'pick')")
+    if input() == "break":
+        print("You break the lock and escape the room, but a guard hears you. You must work quickly to espace the building.")
+    else:
+        print("You try to pick the lock and escape the room.")
+    if input() == "north":
+        print("You walk through the door and find yourself in a room with a table. The table has a note on it. The note says: 'You have been captured by the Celestial Jungle.You will be fed to our leader, the Celestial Dragon. There is another door to the north. Do you want to go try and open it or go back to the hallway? (type 'north' or 'south')")
+        if input() == "north":
+            print("The door is locked. You try to pick the lock, but it is too difficult. You go back to the hallway.")
         else:
-            print("You start feel around for any objects you can find. You find an oddly shaped lock on the door. You feel the lock and feel a puzzle.")
+            print("You made it outside! You are free to roam the jungle, but be aware of the Celestial Dragon and its Celestial Guardians.")
+    else:
+        print("You made it outside! You are free to roam the jungle, but be aware of the Celestial Dragon and its Celestial Guardians.")
+    print("Outside, it is night time. You see a light in the distance. You walk towards the light and find a cave. You walk inside and find a treasure chest. You open the chest and find a . You take the sword and leave the cave. You walk back to the Celestial Jungle and find a Celestial Dragon. You fight the dragon and win. You are now the ruler of the Celestial Jungle.")
+    
