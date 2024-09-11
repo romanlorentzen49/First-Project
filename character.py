5bc5118ae0fbc326f3ad59404ca1746a4d20f7b8
Roman Lorentzen <romanlorentzen49@gmail.com>
2024-09-11 05:35:32 +0000

    Almost done

--- character.py
+++ character.py
@@ -41,6 +41,7 @@ def kidnap():
     print("You made it outside! You are free to roam the jungle, but be aware of the Celestial Dragon and its Celestial Guardians.")
 
 def outside():
+    #have sword
     print("You see a cave. You walk inside and find a treasure chest. You open the chest and find" + item1.description + ". You take the "+ item1.name +". Do you want to continue into the cave or go back to the Celestial Jungle? (type 'cave' or 'jungle')")
     if input() == "cave":
         print("You continue walking through the cave. You see some vines towards the end of the cave. You walk towards the vines. Do you want to cut the vines or go back to the jungle. (type 'cut' or 'leave')")
@@ -60,28 +61,45 @@ def outside():
                 print("The dragon has " + str(Player.health) + " health left.")
                 print("You lose the game!")
         else:
+            #have sword
             print("You continue exploring the jungle. You an animal that isn't a typical animal you would ever see on Earth. What do you want to do examine it or leave it? (type 'examine' or 'leave')")
             if input() == "examine":
                 print("You examine the animal and find that it can heal itself almost instantly about 20 health points. You taser the animal and kill it and pick up its skin which heals you when you eat it. You see a beam of light do you want to go towards the beam of light or go to the cave. (type 'beam' or 'cave')")
+                #have skin
                 if input() == "beam":
-                    print("You walk towards the beam of light and find yourself in a room with a table. The table has a note on it. The note says: You have been captured by the Celestial Jungle.You will be fed to our leader, the Celestial Dragon. There is another door to the north. Do you want to go try and open it or go back to the hallway? (type 'north' or 'south')")
-            else:
+                    print("You walk towards the beam of light and are met with a mountain. You notice there is a cave that seems to go through the mountain. Do you want to go around the mountain or through the cave. (type 'around' or 'cave')") 
+                    if input() == "around":
+                        print("You walk around the mountain. And ")
+                    else:
+                        print("You walk over the mountain and get a good viewpoint to see what the beam of light is. You see that it is a Celestial Dragon. You walk towards the dragon and fight it with the weapons you have. You have ")
+                else:
+                    print("You go into the cave and see vines. Do you want to cut through the vines or go back to the jungle and around the mountain. (type 'cut' or 'around')")
+                    if input() == "cut":
+                        print("You cut the vines and continue through the cave and find the Celestial Dragon. You fight the Celestrial Dragon which has " + str(Dragon.health) +" health with your "+ item1.name +" which does " + str(item1.damage) +" damage. You take the first shot but miss. After missing you try a jump attack and land an attack on the Celestial Dragon.")
+                        Dragon.health =  (Dragon.health - 50)
+                        print("The  Celestian Dragon has " + str(Dragon.health) + " health left.")
+                        print("With that attack, you made the Celestial Dragon mad at you. He breathes fire at you and you take You have " + str(Player.health) + " health left. Do you want to use your "+ skin1.name +" to heal yourself "+ str(skin1.damage) + " or try to hit the dragon with your "+ item1.name +" again. (type 'heal' or 'attack')")
+                        if input() == "heal":
+                            print("You use your "+ skin1.name +" to heal yourself "+ skin1.damage + ".")
+                            Player.health =  (Player.health + skin1.damage)
+                            print("You have " + str(Player.health) + " health left.")
+                            print("You try to hit the Celestial Dragon and you land a hit. The Celestial Dragon is seriously hurt and falls to the ground. You win the game!")
+                        else:
+                            print("You try and attack the dragon but miss. The dragon hits try to hit you back but you dodge it luckily. You notice the dragon is starting to open its mouth to breathe fire at you. Do you want to try and jump attack the dragon again or try and through the "+ item1.name +" into the dragon's mouth and try and stab it that way. (type 'jump' or 'throw')")
+                            if input() == "jump":
+                                print("You jump onto the Dragon and finish it once and for all. You win the game!")
+                            else:
+                                print("You throw the "+ item1.name +" into the dragon's mouth, but the dragon eats the "+ item1.name +" and breathes fire at you and you die.")
+                        
+            else:#have sword
                 print("You leave the animal and continue exploring the jungle. You see a beam of light in the distance. You walk towards the light and notice it goes over a mountain. Do you want to go around the mountain or over the mountin? (type 'around' or 'over')")
                 if input() == "around":
                     print("You walk around the mountain.")
                 else:
                     print("You walk over the mountain and get a good viewpoint to see what the beam of light is. You see that it is a Celestial Dragon. You walk towards the dragon and fight it with the weapons you have. You have ")
     else:
-        print("You walk back to the Celestial Jungle. You see a Celestial Guard. Do you want to fight the guard or continue exploring the jungle? (type 'fight' or 'explore')")
+        print("You walk back to the Celestial Jungle. You see a Celestial Guard. Do you want to fight the guard or try and sneak past the guard and continue exploring? (type 'fight' or 'sneak')")
         if input() == "fight":
             print("You fight the guard and luckily win without anything but your fists and find some sort of celestial taser. But you have a limited time before the guard must check in. You pick up a taser off of the guard.")
         else:
-            print("You continue exploring the jungle. You an animal that isn't a typical animal you would ever see on Earth. What do you want to do examine it or leave it? (type 'examine' or 'leave')")
-            if input() == "examine":
-                print("You examine the animal and find that it can heal itself almost instantly about 20 health points. You taser the animal and kill it.")
-            else:
-                print("You leave the animal and continue exploring the jungle. You see a beam of light in the distance. You walk towards the light and notice it goes over a mountain. Do you want to go around the mountain or over the mountin? (type 'around' or 'over')")
-                if input() == "around":
-                    print("You walk around the mountain.")
-                else:
-                    print("You walk over the mountain and get a good viewpoint to see what the beam of light is. You see that it is a Celestial Dragon. You walk towards the dragon and fight it. The only weapon you have is a ")
+            print("You try to sneak past the guard but the guard sees you and you get captured. You have a bag over your head so you don't know where you are going. When the mask is taken off of you, you see the Celestial Dragon. You can't fight back since you are tied down. The Celestial Dragon eats you.")
--- items.py
+++ items.py
@@ -19,3 +19,11 @@ class taser():
         self.description = description
         self.damage = damage
 taser1 = taser("taser", "a taser that can be used to stun an enemy and electricute them", 20)
+
+class skin():
+    def __init__(self, name, description, damage):
+        self.name = name
+        self.description = description
+        self.damage = damage
+
+skin1 = skin("animal skin", "a skin of an animal that heals you", 20)
